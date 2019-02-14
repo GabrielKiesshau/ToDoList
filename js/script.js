946eceb859b57c4cf2181500ae585dc6dbb22316
Gabriel <gah.reddo@gmail.com>
2019-02-14 20:09:46 -0200

    Final touches  - Put the icon and todo text inside span elements;  - Fixed some positioning;  - Added transition to the trash icon;

--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@ $("#toDoList").on("click", "li", function()
     $(this).toggleClass("done");
 });
 
-$("#toDoList").on("mouseenter", "li", function()
-{
-    console.log($(this).children("i"));
-});
-
 $("#toDoList").on("click", ".deleteIcon", (function(event)
 {
     $(this).parent().fadeOut("1500", function()
@@ -21,11 +16,11 @@ $("input").keypress(function(event)
 {
     if (event.which === 13 && $("input").val() != "")
     {
-        let trashIcon = '<i class="fas fa-trash-alt deleteIcon"></i>';
+        let trashIcon = '<span class="deleteIcon"><i class="fas fa-trash-alt"></i></span>';
         let todoText = $("input").val();
-        
-        let todo = `<li>${trashIcon} ${todoText}</li>`;
 
+        let todo = `<li>${trashIcon}<span class="todo">${todoText}</span></li>`;
+        
         $("#toDoList").append(todo);
 
         $("input").val("");
